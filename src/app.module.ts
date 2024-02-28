773917ad016954f0222cbc6c945a50855a21bec9
AFVA <avillarraga@opc.com.co>
2024-02-28 10:51:49 -0500

    practica de useFactory y uso de la libreria de HttpModule para el manejo de peticiones externas

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
+import { HttpModule, HttpService  } from '@nestjs/axios'
+import { lastValueFrom } from 'rxjs'
 import { ProductsModule } from './products/products.module'
 
 @Module({
-  imports: [ProductsModule],
+  imports: [HttpModule,ProductsModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: 'APP_NAME',
+    useValue: 'NestJS'
+  },
+  {
+    provide: 'TASKS',
+    useFactory: async (http:HttpService) => {
+      const request = http.get('https://jsonplaceholder.typicode.com/todos')
+      const tasks = await lastValueFrom(request)
+      return tasks.data
+    },
+    inject: [HttpService]
+  }],
 })
 export class AppModule {}
